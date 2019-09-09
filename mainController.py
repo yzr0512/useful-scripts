63461a8cfe77491c7bb4fee7c7f9e0baad962fa3
Yzr0512 <yzr20121216@gmail.com>
2019-09-09 13:26:02 +0800

    修改了脚本的主控制器，win跟mac端执行的步骤可以不相同。

--- mainController.py
+++ mainController.py
@@ -1,4 +1,5 @@
 import sys
+import platform
 from syncClipboard import syncClipboard
 from notifier import notifier
 from picbed import picbed
@@ -8,14 +9,34 @@ if __name__ == "__main__":
 		print("The number of parameters is invalid.")
 		exit()
 
+	s = platform.system()
 	if sys.argv[1] == '1':
 		syncClipboard('upload')
-		notifier('云剪贴板', '上传完成。')
+		
+		if s == 'Windows':
+			# Windows
+			pass
+		elif s == 'Darwin':
+			# macOS
+			notifier('云剪贴板', '上传完成。')
+
 	elif sys.argv[1] == '2':
 		syncClipboard()
-		notifier('云剪贴板', '下载完成。')
+		
+		if s == 'Windows':
+			# Windows
+			pass
+		elif s == 'Darwin':
+			# macOS
+			notifier('云剪贴板', '下载完成。')
+
 	elif sys.argv[1] == '3':
 		picbed()
 		syncClipboard('upload')
-		notifier('图床', '上传完成。')
-		
+
+		if s == 'Windows':
+			# Windows
+			pass
+		elif s == 'Darwin':
+			# macOS
+			notifier('图床', '上传完成。')
